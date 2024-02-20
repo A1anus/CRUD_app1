02fb276104855c492a083cefb0768ef54ddfcbb9
Alan <falase07@yandex.ru>
2024-02-20 21:48:46 +0300

    age, email

--- src/main/java/ru/alan/crudapp1/dao/PersonDAO.java
+++ src/main/java/ru/alan/crudapp1/dao/PersonDAO.java
@@ -13,10 +13,10 @@ public class PersonDAO {
 	{
 		people = new ArrayList<>();
 
-		people.add(new Person(++PEOPLE_COUNT, "Alan"));
-		people.add(new Person(++PEOPLE_COUNT, "Valera"));
-		people.add(new Person(++PEOPLE_COUNT, "Dima"));
-		people.add(new Person(++PEOPLE_COUNT, "Katy"));
+		people.add(new Person(++PEOPLE_COUNT, "Alan", 14, "kot@gmail.com"));
+		people.add(new Person(++PEOPLE_COUNT, "Valera", 43, "tok@yandex.ru"));
+		people.add(new Person(++PEOPLE_COUNT, "Dima", 23, "vantzlata@mail.ru"));
+		people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "nastya@key.te"));
 	}
 
 	public List<Person> index() {
@@ -30,5 +30,16 @@ public class PersonDAO {
 	public void save(Person person) {
 		person.setId(++PEOPLE_COUNT);
 		people.add(person);
+	}
+
+	public void update(int id, Person updatePerson) {
+		Person personToBeUpdated = show(id);
+		personToBeUpdated.setName(updatePerson.getName());
+		personToBeUpdated.setAge((updatePerson.getAge()));
+		personToBeUpdated.setEmail(updatePerson.getEmail());
+	}
+
+	public void delete(int id) {
+		people.removeIf(p -> p.getId() == id);
 	}
 }
--- src/main/java/ru/alan/crudapp1/models/Person.java
+++ src/main/java/ru/alan/crudapp1/models/Person.java
@@ -3,10 +3,35 @@ package ru.alan.crudapp1.models;
 public class Person {
 	private String name;
 	private int id;
+	private int age;
+	private String email;
+
+	public Person(int id, String name, int age, String email) {
+		this.name = name;
+		this.id = id;
+		this.age = age;
+		this.email = email;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 	public Person() {
 
-	}
+	 }
 	public Person(int id, String name) {
 		this.id = id;
 		this.name = name;
